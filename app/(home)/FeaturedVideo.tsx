3729461acabf79f1bce9bdf1fb5fe4b5a8843dbd
Hasan <dev.hahuaz@gmail.com>
2023-07-10 11:21:57 +0300

    fix

--- app/(home)/FeaturedVideo.tsx
+++ app/(home)/FeaturedVideo.tsx
@@ -6,13 +6,12 @@ export default function FeaturedVideo() {
       <h1 className='text-2xl text-github-white-link my-1'>Featured Video</h1>
       <iframe
         width='100%'
-        height='500'
-        src='https://www.youtube.com/embed/1GMROn7Wxsc?rel=0'
+        height='400'
+        src='https://www.youtube.com/embed/1GMROn7Wxsc?rel=0&showinfo=0&modestbranding=1'
         title='YouTube video player'
         frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
         allowFullScreen
-        modestbranding='1'
       ></iframe>
     </div>
   );
