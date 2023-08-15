8a1d51d6aec3d1b62d1ba59fac74207f4a27c5d9
Hasan <dev.hahuaz@gmail.com>
2023-08-15 12:59:25 +0300

    hot

--- backend/lambdas/example/index.ts
+++ backend/lambdas/example/index.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/require-await */
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { default as axios } from 'axios';
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { default as axios } from "axios";
 
 export async function main(
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> {
-  console.log('event 👉', event);
+  console.log("incoming event", event);
 
   const { data } = await axios.get(
-    'https://jsonplaceholder.typicode.com/todos/1'
+    "https://jsonplaceholder.typicode.com/todos/1"
   );
   console.log(data);
 
   return {
-    body: JSON.stringify({ message: 'Successful lambda invocation' }),
+    body: JSON.stringify({ message: "Successful lambda invocation" }),
     statusCode: 200,
   };
 }
